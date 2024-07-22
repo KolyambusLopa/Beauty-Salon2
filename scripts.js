906b0ad0aedd37f8071ad81fb5af924251b10565
KolyambusLopa <akuzolna@gmail.com>
2024-07-22 13:27:42 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,29 +1,24 @@
-console.log('JavaScript загружен');
-
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const menu = document.getElementById('menu');
-    const closeBtn = document.getElementById('close-btn');
+    const closeBtn = document.querySelector('.close-btn');
 
     if (hamburger && menu && closeBtn) {
         hamburger.addEventListener('click', () => {
             menu.classList.toggle('show');
             closeBtn.style.display = menu.classList.contains('show') ? 'block' : 'none';
-            hamburger.classList.toggle('disabled', menu.classList.contains('show'));
         });
 
         closeBtn.addEventListener('click', () => {
             menu.classList.remove('show');
             closeBtn.style.display = 'none';
-            hamburger.classList.remove('disabled');
         });
 
         // Закрытие меню при клике вне его области
         document.addEventListener('click', (event) => {
-            if (!hamburger.contains(event.target) && !menu.contains(event.target) && !closeBtn.contains(event.target)) {
+            if (!hamburger.contains(event.target) && !menu.contains(event.target)) {
                 menu.classList.remove('show');
                 closeBtn.style.display = 'none';
-                hamburger.classList.remove('disabled');
             }
         });
     } else {
