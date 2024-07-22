080f1900f58373354842416acac7d44bc859055b
KolyambusLopa <akuzolna@gmail.com>
2024-07-22 19:32:02 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const menu = document.getElementById('menu');
+    const closeMenu = document.getElementById('close-menu');
 
-    if (hamburger && menu) {
+    if (hamburger && menu && closeMenu) {
         hamburger.addEventListener('click', () => {
             menu.classList.toggle('show');
         });
 
-        // Закрытие меню при клике вне его области
+        closeMenu.addEventListener('click', () => {
+            menu.classList.remove('show');
+        });
+
         document.addEventListener('click', (event) => {
             if (!hamburger.contains(event.target) && !menu.contains(event.target)) {
                 menu.classList.remove('show');
             }
         });
-    } else {
-        console.error('Элементы не найдены');
     }
 });
