86710d388a70cff5e03a21e7e1aaa08554d3b16f
KolyambusLopa <akuzolna@gmail.com>
2024-07-22 20:41:10 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const menu = document.getElementById('menu');
-    const closeMenu = document.getElementById('close-menu');
 
-    if (hamburger && menu && closeMenu) {
-        hamburger.addEventListener('click', () => {
-            menu.classList.toggle('show');
-        });
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('show');
+    });
 
-        closeMenu.addEventListener('click', () => {
+    // Закрытие меню при клике вне его области (если нужно)
+    document.addEventListener('click', (event) => {
+        if (!hamburger.contains(event.target) && !menu.contains(event.target)) {
             menu.classList.remove('show');
-        });
-
-        document.addEventListener('click', (event) => {
-            if (!hamburger.contains(event.target) && !menu.contains(event.target) && !closeMenu.contains(event.target)) {
-                menu.classList.remove('show');
-            }
-        });
-    } else {
-        console.error('Элементы не найдены');
-    }
+        }
+    });
 });
