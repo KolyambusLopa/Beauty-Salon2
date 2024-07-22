af90c9e8d1d4a134f17bd20e699dd563fd492d92
KolyambusLopa <akuzolna@gmail.com>
2024-07-22 12:20:59 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,13 +2,19 @@ console.log('JavaScript загружен');
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const menu = document.getElementById('menu');
+    const closeBtn = document.getElementById('close-btn');
 
-    if (hamburger && menu) {
+    if (hamburger && menu && closeBtn) {
         console.log('JavaScript загружен и элементы найдены');
 
         hamburger.addEventListener('click', () => {
             menu.classList.toggle('show');
             console.log('Hamburger clicked');
+        });
+
+        closeBtn.addEventListener('click', () => {
+            menu.classList.remove('show');
+            console.log('Close button clicked');
         });
 
         // Закрытие меню при клике вне его области
