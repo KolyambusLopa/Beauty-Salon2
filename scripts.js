565411fe20a328f650cd2a79104c29d4692a789b
KolyambusLopa <akuzolna@gmail.com>
2024-07-22 13:09:25 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+console.log('JavaScript загружен');
+
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const menu = document.getElementById('menu');
@@ -7,11 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
         hamburger.addEventListener('click', () => {
             menu.classList.toggle('show');
             closeBtn.style.display = menu.classList.contains('show') ? 'block' : 'none';
+            hamburger.classList.toggle('disabled', menu.classList.contains('show'));
         });
 
         closeBtn.addEventListener('click', () => {
             menu.classList.remove('show');
             closeBtn.style.display = 'none';
+            hamburger.classList.remove('disabled');
         });
 
         // Закрытие меню при клике вне его области
@@ -19,7 +23,10 @@ document.addEventListener('DOMContentLoaded', () => {
             if (!hamburger.contains(event.target) && !menu.contains(event.target) && !closeBtn.contains(event.target)) {
                 menu.classList.remove('show');
                 closeBtn.style.display = 'none';
+                hamburger.classList.remove('disabled');
             }
         });
+    } else {
+        console.error('Элементы не найдены');
     }
 });
