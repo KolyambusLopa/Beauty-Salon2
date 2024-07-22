cbac2ac10348c51fba45df3cf652241cef9240c0
KolyambusLopa <akuzolna@gmail.com>
2024-07-22 22:56:56 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,18 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const menu = document.getElementById('menu');
 
     if (hamburger && menu) {
-        console.log('JavaScript загружен и элементы найдены');
-
         hamburger.addEventListener('click', () => {
             menu.classList.toggle('show');
-            console.log('Hamburger clicked');
         });
 
-        // Закрытие меню при клике вне его области
         document.addEventListener('click', (event) => {
             if (!hamburger.contains(event.target) && !menu.contains(event.target)) {
                 menu.classList.remove('show');
-                console.log('Clicked outside menu');
             }
         });
     } else {
