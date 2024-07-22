3cb478f911a1edccf87f6d75a0e60c65465b5f1e
KolyambusLopa <akuzolna@gmail.com>
2024-07-22 13:40:14 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const menu = document.getElementById('menu');
-    const closeBtn = document.querySelector('.close-btn');
 
-    if (hamburger && menu && closeBtn) {
+    if (hamburger && menu) {
         hamburger.addEventListener('click', () => {
             menu.classList.toggle('show');
-            closeBtn.style.display = menu.classList.contains('show') ? 'block' : 'none';
-        });
-
-        closeBtn.addEventListener('click', () => {
-            menu.classList.remove('show');
-            closeBtn.style.display = 'none';
         });
 
         // Закрытие меню при клике вне его области
         document.addEventListener('click', (event) => {
-            if (!hamburger.contains(event.target) && !menu.contains(event.target) && !closeBtn.contains(event.target)) {
+            if (!hamburger.contains(event.target) && !menu.contains(event.target)) {
                 menu.classList.remove('show');
-                closeBtn.style.display = 'none';
             }
         });
     } else {
