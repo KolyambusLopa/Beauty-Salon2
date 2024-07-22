8d755df287c94aa3e6ab306fccc6b79f448f21b6
KolyambusLopa <akuzolna@gmail.com>
2024-07-22 23:26:25 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,11 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
     if (hamburger && menu) {
         hamburger.addEventListener('click', () => {
             menu.classList.toggle('show');
+            hamburger.classList.toggle('active'); // Toggle active class on hamburger
         });
 
         document.addEventListener('click', (event) => {
             if (!hamburger.contains(event.target) && !menu.contains(event.target)) {
                 menu.classList.remove('show');
+                hamburger.classList.remove('active'); // Remove active class on hamburger
             }
         });
     } else {
