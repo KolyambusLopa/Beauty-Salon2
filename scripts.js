cf8e9409917a17143eebac14dff2ba4a6606792a
KolyambusLopa <akuzolna@gmail.com>
2024-07-22 12:56:08 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,31 +1,22 @@
-console.log('JavaScript загружен');
-
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const menu = document.getElementById('menu');
     const closeBtn = document.getElementById('close-btn');
 
     if (hamburger && menu && closeBtn) {
-        console.log('JavaScript загружен и элементы найдены');
-
         hamburger.addEventListener('click', () => {
             menu.classList.toggle('show');
-            console.log('Hamburger clicked');
         });
 
         closeBtn.addEventListener('click', () => {
             menu.classList.remove('show');
-            console.log('Close button clicked');
         });
 
         // Закрытие меню при клике вне его области
         document.addEventListener('click', (event) => {
             if (!hamburger.contains(event.target) && !menu.contains(event.target)) {
                 menu.classList.remove('show');
-                console.log('Clicked outside menu');
             }
         });
-    } else {
-        console.error('Элементы не найдены');
     }
 });
